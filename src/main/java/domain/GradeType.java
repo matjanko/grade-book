699bcffa3d1bb63abadc9f134c85540a2971c707
matjanko <mateusz.jankowski@prokonto.pl>
2019-07-12 11:26:23 +0200

    Implement grade types

--- src/main/java/domain/GradeType.java
+++ src/main/java/domain/GradeType.java
@@ -1,5 +1,20 @@
 package domain;
 
 public enum GradeType {
+    UNSATISFACTORY(1),
+    POOR(2),
+    SATISFACTORY(3),
+    GOOD(4),
+    VERY_GOOD(5),
+    EXCELLENT(6);
 
+    private int numberValue;
+
+    GradeType(int numberValue) {
+        this.numberValue = numberValue;
+    }
+
+    public int getNumberValue() {
+        return numberValue;
+    }
 }
